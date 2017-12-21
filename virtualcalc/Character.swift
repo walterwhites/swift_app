87828cd796d2d7bd5ac5a3dd42fb9bd30c57fca4
flo <hopemagie@gmail.com>
2017-12-21 08:46:12 -0800

    second commit

---
+++ virtualcalc/Character.swift
@@ -0,0 +1,16 @@
+//
+//  Character.swift
+//  virtualcalc
+//
+//  Created by flo on 21/12/2017.
+//  Copyright © 2017 Florent. All rights reserved.
+//
+
+
+import Foundation
+
+struct Character {
+    let imageName: String!
+    let name: String!
+    let movie: String!
+}
--- virtualcalc/ViewController.swift
+++ virtualcalc/ViewController.swift
@@ -7,54 +7,117 @@
 //
 
 import UIKit
+import UPCarouselFlowLayout
+import CoreGraphics
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var currentValue: UILabel!
-    @IBOutlet weak var resultValue: UILabel!
     
-    private var storeOperateur = ""
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
     
-    @IBAction func getChiffre(_ sender: UIButton) {
-        if let chiffre = sender.title(for: .normal) {
-            currentValue.text =   currentValue.text != "0"
-                                ? currentValue.text! + chiffre
-                                : chiffre
+    fileprivate var items = [Character]()
+    
+    fileprivate var currentPage: Int = 0 {
+        didSet {
+            let character = self.items[self.currentPage]
+            self.infoLabel.text = character.name.uppercased()
+            self.detailLabel.text = character.movie.uppercased()
         }
     }
     
-    @IBAction func operateur(_ sender: UIButton) {
-        if let buttonLabel = sender.title(for: .normal) {
-            
-            if buttonLabel == "clear" {
-                currentValue.text = "0"
-                resultValue.text  = "0"
-            } else {
-                var calcul:Float = 0
-                switch self.storeOperateur {
-                    //case "+": calcul = Float(resultValue.text!)! + Float(currentValue.text!)!
-                    case "-": calcul = Float(resultValue.text!)! - Float(currentValue.text!)!
-                    case "*": calcul = Float(resultValue.text!)! * Float(currentValue.text!)!
-                    case "/": calcul = Float(resultValue.text!)! / Float(currentValue.text!)!
-                    default : calcul = Float(resultValue.text!)! + Float(currentValue.text!)!
-                }
-
-                resultValue.text  = calcul.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", calcul) : String(calcul)
-                currentValue.text = "0"
-                self.storeOperateur = buttonLabel
-            }
-            
+    fileprivate var pageSize: CGSize {
+        let layout = self.collectionView.collectionViewLayout as! UPCarouselFlowLayout
+        var pageSize = layout.itemSize
+        if layout.scrollDirection == .horizontal {
+            pageSize.width += layout.minimumLineSpacing
+        } else {
+            pageSize.height += layout.minimumLineSpacing
         }
+        return pageSize
     }
-
+    func CGSizeMake(_ width: CGFloat, _ height: CGFloat) -> CGSize {
+        return CGSize(width: width, height: height)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        resultValue.text  = "0"
-        currentValue.text = "0"
         
-        // Do any additional setup after loading the view, typically from a nib.
+        self.setupLayout()
+        self.items = self.createItems()
+        
+        self.currentPage = 0
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.rotationDidChange), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
     }
-
+    
+    fileprivate func setupLayout() {
+        let layout = self.collectionView.collectionViewLayout as! UPCarouselFlowLayout
+        layout.spacingMode = UPCarouselFlowLayoutSpacingMode.overlap(visibleOffset: 30)
+    }
+    
+    fileprivate func createItems() -> [Character] {
+        let characters = [
+            Character(imageName: "wall-e", name: "Wall-E", movie: "Wall-E"),
+            Character(imageName: "nemo", name: "Nemo", movie: "Finding Nemo"),
+            Character(imageName: "ratatouille", name: "Remy", movie: "Ratatouille"),
+            Character(imageName: "buzz", name: "Buzz Lightyear", movie: "Toy Story"),
+            Character(imageName: "monsters", name: "Mike & Sullivan", movie: "Monsters Inc."),
+            Character(imageName: "brave", name: "Merida", movie: "Brave")
+        ]
+        return characters
+    }
+    
+    fileprivate var orientation: UIDeviceOrientation {
+        return UIDevice.current.orientation
+    }
+    
+    @objc fileprivate func rotationDidChange() {
+        guard !orientation.isFlat else { return }
+        let layout = self.collectionView.collectionViewLayout as! UPCarouselFlowLayout
+        let direction: UICollectionViewScrollDirection = UIDeviceOrientationIsPortrait(orientation) ? .horizontal : .vertical
+        layout.scrollDirection = direction
+        if currentPage > 0 {
+            let indexPath = IndexPath(item: currentPage, section: 0)
+            let scrollPosition: UICollectionViewScrollPosition = UIDeviceOrientationIsPortrait(orientation) ? .centeredHorizontally : .centeredVertically
+            self.collectionView.scrollToItem(at: indexPath, at: scrollPosition, animated: false)
+        }
+    }
+    
+    // MARK: - Card Collection Delegate & DataSource
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CarouselCollectionViewCell.identifier, for: indexPath) as! CarouselCollectionViewCell
+        let character = items[(indexPath as NSIndexPath).row]
+        cell.image.image = UIImage(named: character.imageName)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let character = items[(indexPath as NSIndexPath).row]
+        let alert = UIAlertController(title: character.name, message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - UIScrollViewDelegate
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let layout = self.collectionView.collectionViewLayout as! UPCarouselFlowLayout
+        let pageSide = (layout.scrollDirection == .horizontal) ? self.pageSize.width : self.pageSize.height
+        let offset = (layout.scrollDirection == .horizontal) ? scrollView.contentOffset.x : scrollView.contentOffset.y
+        currentPage = Int(floor((offset - pageSide / 2) / pageSide) + 1)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
